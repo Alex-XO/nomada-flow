14c761c3255854447cf43b681eaf6d3c17838b35
Alex <s2a5s0h6a@mail.ru>
2024-11-18 21:57:30 +0100

    Initial commit

---
+++ src/main/kotlin/com/example/nomadaflow/NomadaFlowApplication.kt
@@ -0,0 +1,11 @@
+package com.example.nomadaflow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NomadaFlowApplication
+
+fun main(args: Array<String>) {
+    runApplication<NomadaFlowApplication>(*args)
+}
